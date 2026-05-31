72dacba9914bd2ef83f3913133da606b3baf5c42
iifar sahdan <namagwpia@gmail.com>
2026-05-31 11:00:52 +0700

    Use DB_CONFIG for database connection

--- app/config.py
+++ app/config.py
@@ -5,10 +5,8 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-DB_CONFIG = {
-    "host": os.getenv("MYSQLHOST"),
-    "user": os.getenv("MYSQLUSER"),
-    "password": os.getenv("MYSQLPASSWORD"),
-    "database": os.getenv("MYSQLDATABASE"),
-    "port": int(os.getenv("MYSQLPORT", 3306)),
-}
+import mysql.connector
+from app.config import DB_CONFIG
+
+def connect_db():
+    return mysql.connector.connect(**DB_CONFIG)
